5803f2fc8b3aa5b21a4b60878ccab79809e270da
Tomdek <tomdek@upcmail.nl>
2016-11-24 15:37:13 +0100

    Ready to start

--- Action_On_Impact/src/GpR2D2.py
+++ Action_On_Impact/src/GpR2D2.py
@@ -8,3 +8,15 @@ import XLoBorg
 import time
 import os
 
+#declare variables
+
+#Loop indefinitely
+#check for changes
+    #get sensor data
+        #log it
+    #if 'hit' detected 
+        #play random sound
+
+#Method: play soundfile
+
+#Log data 
